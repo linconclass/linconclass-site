cb3b00ca9a8648c4b5fb96fc06038d664e5f18fb
Lincon Class <65132985+linconclass@users.noreply.github.com>
2026-08-30 17:23:58 -0300

    Load card media styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./card-media.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 
